e55f4c1ad4dba1a5b8050ef8d8d912d2260a8fc3
rpaized <sitkovskiy.gleb@gmail.com>
2022-03-24 23:43:09 +0700

    добавлена четвертная оценка 3, почищен код

--- src/score.js
+++ src/score.js
@@ -7,6 +7,7 @@ const quarterFiveEstimation = 4.56 // Оценка 5 четвертная
 let writeQuantity = 0; // Содержит тип вводимых данных
 let arrayQuantityString = []; // Соддержит кол-во оценок
 let askWriteQuantity = confirm(`Нажмите "Да", если хотите вводить данные через пробел, или "Нет", если хотите вводить данные слитно.`);
+
 // Опрос типа вводимых данных
 if (askWriteQuantity == true) { 
     writeQuantity = prompt(`Введите оценки через пробел`);
@@ -26,7 +27,7 @@ let arrayQuantity = arrayQuantityString.map(function (item) {
     return parseFloat(item);
   }); 
 
-const initialValue = 0; // Первоначальное значение
+  const initialValue = 0; // Первоначальное значение
 
 // Сумма оценок
 const sumQuantity = arrayQuantity.reduce( 
@@ -36,17 +37,6 @@ const sumQuantity = arrayQuantity.reduce(
 
 const amountScoreInArray = Object.keys(arrayQuantity).length; // Кол-во оценок
 const middleScore = sumQuantity/amountScoreInArray; // Средний балл предмета (вводимого пользователем)
-
-// Хранилища для кол-ва нужных оценок
-// Для тройки
-let storageFirst1 = 0;
-let storageFirst2 = 0;
-// Для четвёртки
-let storageSecond1 = 0;
-let storageSecond2 = 0;
-// Для пятёрки
-let storageThird1 = 0;
-let storageThird2 = 0; 
 
 // Функция - подсчёт кол-ва оценок для получения нужной оценки в четверти
 function countScore(quarterEstimation, estimation) {
@@ -64,12 +54,16 @@ function countScore(quarterEstimation, estimation) {
 }
 
 // Функция - сообщение кол-ва оценок для нужной четвертной оценки
-function alertEstimation(storage1, storage2, storage3, storage4) {
-    alert("Ваш средний балл: " + middleScore.toFixed(2) + "\nЧтобы за четверть вышла 4, нужно 4: " + storage1 + 
-    ", или 5: " + storage2 + "\nЧтобы за четверть вышла 5, нужно 5: " + storage3);
+function showEstimation(storage1, storage2, storage3, storage4, storage5) {
+    alert("Ваш средний балл: " + middleScore.toFixed(2) + "\nЧтобы за четверть вышла 3, нужно 4: " + storage1 + 
+    ", или 5: " + storage2 + "\nЧтобы за четверть вышла 4, нужно 4: " + storage3 + ", или 5: " + storage4 + 
+    "\nЧтобы за четверть вышла 5, нужно 5: " + storage5);
 }
   
-alertEstimation(countScore(quarterFourEstimation, 4), 
-                countScore(quarterFourEstimation, 5),
-                countScore(quarterFiveEstimation, 5));
+showEstimation(countScore(quarterThreeEstimation, 4), // storage1 (четвертная 3)
+               countScore(quarterThreeEstimation, 5), // storage2 (четвертная 3)
+               countScore(quarterFourEstimation, 4),  // storage3 (четвертная 4)
+               countScore(quarterFourEstimation, 5),  // storage4 (четвертная 4)
+               countScore(quarterFiveEstimation, 5)   // storage5  (четвертная 5)
+);
 
